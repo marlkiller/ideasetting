9cb3306c696a1501210b2b8ce2715c940598fec0
ma.zhiwu <ma.zhiwu@99cloud.net>
2021-12-31 09:28:08 +0800

    IU-2021.3 <artemis@artemisdeMacBook-Pro.local Create markdown.xml, project.default.xml, diff.xml, equalsHashCodeTemplates.xml, Enum.java, Interface.java, Servlet Annotated Class.java, Default.xml, visualizationTool.xml, find.xml, File Header.java, Filter Class.java, AndroidLog.xml, gerrit_settings.xml, _@user_Darcula.icls, gutter.xml, GrepConsole.xml, RestfulToolSetting.xml, Default.xml, docker.xml, keymap.xml, editor.xml, debugger.xml, updates.xml, baseRefactoring.xml, ide.general.xml, other.xml, advancedSettings.xml, lombok-plugin.xml, user.xml, git.xml, shared-indexes.xml, ignored-suggested-plugins.xml, ui.lnf.xml, filetypes.xml, projectView.xml, remote-servers.xml, plugin_ui.xml, colors.scheme.xml, notifications.xml, databaseDrivers.xml, github.xml, templates.xml, AnnotationType.java, Logger.xml, laf.xml, sshConfigs.xml, Servlet Class.java, parameter.hints.xml, Class.java, jdk.table.xml, Filter Annotated Class.java, acceptedLanguageLevels.xml, intentionSettings.xml, vcs.xml, docker-registry.xml, grazie_global.xml, toStringTemplates.xml, usageView.xml, Mac OS X copy.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,13 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")
+    package ${PACKAGE_NAME};
+#end
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author ${USER}
+ * @date ${DATE}
+ */
+public class ${NAME} {
+
+}
