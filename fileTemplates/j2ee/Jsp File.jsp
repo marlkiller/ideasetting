8e1a964b4b8672b4ae6c810aab7522d53feb4f45
marlk <marlk@DESKTOP-1BJ2FP5.lan>
2022-01-01 14:16:41 +0800

    IU-2021.3.1 <marlk@DESKTOP-1BJ2FP5 Create markdown.xml, IntelliLang.xml, print.xml, project.default.xml, diff.xml, equalsHashCodeTemplates.xml, Enum.java, Interface.java, Darcula black.icls, Servlet Annotated Class.java, Default.xml, Default gray.icls, find.xml, File Header.java, Filter Class.java, androidEditors.xml, code.style.schemes.xml, Keymap DIY.xml, Default.xml, JavaScript.xml, plain.xml, editor.xml, debugger.xml, MyGroup.xml, updates.xml, baseRefactoring.xml, ide.general.xml, other.xml, ide-features-trainer.xml, lombok-plugin.xml, shared-indexes.xml, androidStudioFirstRun.xml, ui.lnf.xml, filetypes.xml, projectView.xml, Default write.icls, plugin_ui.xml, colors.scheme.xml, notifications.xml, databaseDrivers.xml, github.xml, templates.xml, AnnotationType.java, xml.xml, laf.xml, Servlet Class.java, spring-beans.schema.xml, studioFlags.xml, parameter.hints.xml, Listener Annotated Class.java, Class.java, jdk.table.xml, Filter Annotated Class.java, Jsp File.jsp, intentionSettings.xml, Default _1_.xml, proxy.settings.xml, tasks.xml, vcs.xml, grazie_global.xml, toStringTemplates.xml, Xml File.xml, usageView.xml

---
+++ fileTemplates/j2ee/Jsp File.jsp
@@ -0,0 +1,14 @@
+<%--
+  User: ${USER}
+  Date: ${DATE}
+  Time: ${TIME}
+--%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<html>
+  <head>
+    <title>#[[$Void,s Jsp$]]#</title>
+  </head>
+  <body>
+  #[[$END$]]#
+  </body>
+</html>
---
+++ fileTemplates/j2ee/Servlet Class.java
@@ -0,0 +1,19 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+#parse("File Header.java")
+public class ${Class_Name} extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        request.setCharacterEncoding("utf-8");
+    }
+}
